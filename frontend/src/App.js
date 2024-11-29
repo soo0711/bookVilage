8286e6e7527838d7112549ecb09d470e28a90d9e
joheeeon <whgmldjs@gmail.com>
2024-11-30 01:59:00 +0900

    메인화면 공지사항까지 완료했습니다..

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+
+import React from "react";
+import Header from "./mainpage/header";
+import Banner from "./mainpage/banner";
+import High from "./mainpage/high";
+import Notice from "./mainpage/Notice";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+       <Banner />
+      <High /> 
+       <Notice /> 
+    </div>
+  );
+}
+
+export default App;
