60c1a0053ef865175efed530a31ae5688b921451
kimchaeyeon <kelly021128@naver.com>
2024-12-03 20:28:56 +0900

    웹소캣 config

---
+++ backend/project_workspace/bookVillage/src/main/java/com/example/bookVillage/message/MessageController.java
@@ -0,0 +1,5 @@
+package com.example.bookVillage.message;
+
+public class MessageController {
+
+}
--- backend/project_workspace/bookVillage/src/main/java/com/example/bookVillage/user/UserController.java
+++ backend/project_workspace/bookVillage/src/main/java/com/example/bookVillage/user/UserController.java
@@ -1,11 +1,13 @@
 package com.example.bookVillage.user;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import jakarta.servlet.http.HttpSession;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/user")
 @Controller
 public class UserController {
