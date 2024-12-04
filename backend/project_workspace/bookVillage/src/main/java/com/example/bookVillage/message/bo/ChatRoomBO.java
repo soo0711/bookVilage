33b43dde9f2240f60d1f5e9e53c7c41cd0a303a2
kimchaeyeon <kelly021128@naver.com>
2024-12-04 22:25:04 +0900

    채팅 구현

---
+++ backend/project_workspace/bookVillage/src/main/java/com/example/bookVillage/message/bo/ChatRoomBO.java
@@ -0,0 +1,8 @@
+package com.example.bookVillage.message.bo;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ChatRoomBO {
+	
+}
--- backend/project_workspace/bookVillage/src/main/java/com/example/bookVillage/message/bo/MessageBO.java
+++ backend/project_workspace/bookVillage/src/main/java/com/example/bookVillage/message/bo/MessageBO.java
@@ -1,5 +1,26 @@
 package com.example.bookVillage.message.bo;
 
-public class MessageBO {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.example.bookVillage.message.entity.MessageEntity;
+import com.example.bookVillage.message.repository.MessageRepository;
+
+@Service
+public class MessageBO {
+	@Autowired
+	private MessageRepository messageRepository;
+	
+	public MessageEntity addMessage(int chatroomId, int userId,String message ) {
+		
+		MessageEntity messageEntity = messageRepository.save(
+				MessageEntity.builder()
+				.chatroomId(chatroomId)
+				.userId(userId)
+				.message(message)
+				.build()
+				);
+		
+		 return messageEntity != null ? messageEntity : null;
+	}
 }
