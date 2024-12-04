e9bb36784255d9c9963dad0dbd5c7ed160f97e6d
kimchaeyeon <kelly021128@naver.com>
2024-12-04 14:28:56 +0900

    채팅 구현

--- backend/project_workspace/bookVillage/src/main/java/com/example/bookVillage/config/WebSocketConfig.java
+++ backend/project_workspace/bookVillage/src/main/java/com/example/bookVillage/config/WebSocketConfig.java
@@ -11,18 +11,14 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-    @Override
+	@Override
     public void registerStompEndpoints(StompEndpointRegistry registry){
-    	// stomp 접속 주소 url = ws://localhost:8080/ws, 프로토콜이 http가 아니다!
         registry.addEndpoint("/ws")     
                 .setAllowedOrigins("*");
     }
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry){
-        // 메시지를 구독(수신)하는 요청 엔드포인트
         registry.enableSimpleBroker("/sub");
-        
-        // 메시지를 발행(송신)하는 엔드포인트
         registry.setApplicationDestinationPrefixes("/pub");
     }
 }
--- backend/project_workspace/bookVillage/src/main/java/com/example/bookVillage/message/MessageController.java
+++ backend/project_workspace/bookVillage/src/main/java/com/example/bookVillage/message/MessageController.java
@@ -1,5 +1,28 @@
 package com.example.bookVillage.message;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MessageController {
 
+	@Autowired
+    private ChatService chatService;
+	
+	@Autowired
+	private SimpMessagingTemplate messagingTemplate;
+	
+	@MessageMapping("/message")
+    @Operation(summary = "메시지 전송", description = "메시지를 전송합니다.")
+    public ResponseEntity<String> receiveMessage(@RequestBody ChatMessageDto messageDto) {
+        // 메시지 저장
+        ChatMessage chatMessage = chatService.saveMessage(messageDto);
+
+        // 메시지를 해당 채팅방 구독자들에게 전송
+        messagingTemplate.convertAndSend("/sub/chatroom/" + messageDto.getChatroomSeq(), messageDto);
+        return ResponseEntity.ok("메시지 전송 완료");
 }
